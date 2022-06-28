8d8b162c8fbfb3c91034a27e0a4d506aaf00da2f
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-06-28 17:19:42 -0300

    check answer
    no anda

--- game trivia/script.js
+++ game trivia/script.js
@@ -1,28 +1,36 @@
+const random = (max) => { return Math.floor(Math.random() * (max - 1)) + 1; }
+question = document.getElementById('question');
+let questions = []
 
-const random = (max) =>{ return Math.floor(Math.random() * (max - 1)) + 1; }
-
-question = document.getElementById('questions');
+axios({
+    method: 'GET',
+    url: 'https://trivia-questions.p.rapidapi.com/trivia',
+    headers: {
+        'X-RapidAPI-Key': 'e4ee98feebmshfc58a78f99420e1p112687jsnee798134c226',
+        'X-RapidAPI-Host': 'trivia-questions.p.rapidapi.com'
+    }
+}).then(response => {
+    questions = response.data
+}).catch(function (error) {
+    console.error(error);
+});
 
 function pickRandom(){
-
-    axios({
-        method: 'GET',
-        url: 'https://trivia-questions.p.rapidapi.com/trivia',
-        headers: {
-            'X-RapidAPI-Key': 'e4ee98feebmshfc58a78f99420e1p112687jsnee798134c226',
-            'X-RapidAPI-Host': 'trivia-questions.p.rapidapi.com'
-        }
-    }).then(response => {
-        console.log(response)
-        num = random(100)
-        
-        question.innerText = `${response.data[num].Question}`
-        answer = document.createElement("input")
-        answer.type = "text"
-        answer.id = "answer"
-
-    }).catch(function (error) {
-        console.error(error);
-    });
+    num = random(100)
+    question.innerText = `${questions[num].Question}`
+    answer = document.createElement("input")
+    answer.type = "text"
+    answer.id = "answer"
 }
 
+function checkAnswer() {
+    answer = document.getElementById('answer')
+    let i =questions.indexOf(Question = question)
+    if (answer = questions[i].Answer ){
+        alert('correcto!')
+    }
+    else{
+        alert('incorrecto')
+    }
+    
+}
