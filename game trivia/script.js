5dc83eed5a8b5d2f6df810d67297ab7f6b9edef7
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-07-06 10:35:51 -0300

    checkanswer not finished yet

--- game trivia/script.js
+++ game trivia/script.js
@@ -1,45 +1,47 @@
-const random = (max) => { return Math.floor(Math.random() * (max - 1)) + 1; }
+const random = (max) => {
+  return Math.floor(Math.random() * (max - 1)) + 1;
+};
 
-let questions = []
+const questions = [];
 
-
-function searchQuestions(){ 
-
-    axios({
-        method: 'GET',
-        url: 'https://trivia-questions.p.rapidapi.com/trivia',
-        headers: {
-            'X-RapidAPI-Key': 'e4ee98feebmshfc58a78f99420e1p112687jsnee798134c226',
-            'X-RapidAPI-Host': 'trivia-questions.p.rapidapi.com'
-        }
-    }).then(response => {
-        questions.push(response.data)
-
-    }).catch(function (error) {
-        console.error(error)
+function searchQuestions() {
+  return axios({
+    method: "GET",
+    url: "https://trivia-questions.p.rapidapi.com/trivia",
+    headers: {
+      "X-RapidAPI-Key": "e4ee98feebmshfc58a78f99420e1p112687jsnee798134c226",
+      "X-RapidAPI-Host": "trivia-questions.p.rapidapi.com",
+    },
+  })
+    .then((response) => {
+      questions.push(...response.data);
+      if (questions.length > 0) {
+        return questions;
+      }
+      console.log(questions);
+      return questions;
     })
-
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
+searchQuestions();
 function pickRandom() {
-    searchQuestions()
-    console.log(questions)
-    let question = document.getElementById('question')
-    num = random(100)
-    question.innerText = `${questions[num]}`
-   
+  let question = document.getElementById("question");
+  num = random(100);
+  question.innerText = `${questions[num].Question}`;
+  return num;
+}
+
+function checkAnswer() {
+  n = pickRandom();
+  let answer = document.getElementById("answer").value;
+  let text = document.getElementById('bool')
+  if ((answer = questions[n].Answer)) {
+    text.innerText = "Correcto";
+  } else {
+    text.innerText = "Incorrecto";
+  }
 
 }
-/*
-function checkAnswer() {
-    question = document.getElementById('question');
-    answer = document.getElementById('answer')
-    let i = questions.indexOf(Question = question)
-    if (answer = questions[i].Answer) {
-        alert('correcto!')
-    }
-    else {
-        alert('incorrecto')
-    }
-
-}*/
